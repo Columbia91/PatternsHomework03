6a1d3337e2d07ecb9c3144558aeffdf7cef200b4
globalmail91@gmail.com <globalmail91@gmail.com>
2019-07-26 14:02:13 +0300

    Добавьте файлы проекта.

---
+++ Adapter/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // человек
+            Human human = new Human();
+            // сканер
+            Scanner scanner = new Scanner();
+            // готовит копию документа
+            human.ToPrepareDocument(scanner);
+            // качество копии не устраивает, но есть смартфон
+            Samsung samsung = new Samsung();
+            // используем адаптер
+            InputDevice smartphone = new Adapter(samsung);
+            // для получения копии, делаем снимок документа на смартфон
+            human.ToPrepareDocument(smartphone);
+
+            Console.Read();
+        }
+    }
+    interface InputDevice
+    {
+        void Scan();
+    }
+    // класс машины
+    class Scanner : InputDevice
+    {
+        public void Scan()
+        {
+            Console.WriteLine("Сканировать документ");
+        }
+    }
+    class Human
+    {
+        public void ToPrepareDocument(InputDevice device)
+        {
+            device.Scan();
+        }
+    }
+    
+    interface SmartPhone
+    {
+        void MakePhoto();
+    }
+    
+    class Samsung : SmartPhone
+    {
+        public void MakePhoto()
+        {
+            Console.WriteLine("Сфотографировать документ");
+        }
+    }
+    
+    class Adapter : InputDevice
+    {
+        Samsung samsung;
+        public Adapter(Samsung s)
+        {
+            samsung = s;
+        }
+
+        public void Scan()
+        {
+            samsung.MakePhoto();
+        }
+    }
+}
---
+++ Decorator/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Component carlson = new Carlson();
+            Decorator overall = new Overall();
+            Decorator propeller = new Propeller();
+
+            overall.Component = carlson;
+            propeller.Component = overall;
+            propeller.Move();
+        }
+
+        abstract class Component
+        {
+            public abstract void Move();
+        }
+
+        class Carlson : Component
+        {
+            public override void Move()
+            {
+                Console.WriteLine("Карлсон проснулся");
+            }
+        }
+        abstract class Decorator : Component
+        {
+            public Component Component { get; set; }
+            public override void Move()
+            {
+                if (Component != null)
+                {
+                    Component.Move();
+                }
+            }
+        }
+        class Overall : Decorator
+        {
+            private void Walk()
+            {
+                Console.WriteLine("Карлсон одел комбинезон и пошел гулять");
+            }
+            public override void Move()
+            {
+                base.Move();
+                Walk();
+            }
+        }
+        class Propeller : Decorator
+        {
+            private void Fly()
+            {
+                Console.WriteLine("Карлсон одел пропеллер и полетел");
+            }
+            public override void Move()
+            {
+                base.Move();
+                Fly();
+            }
+        }
+    }
+}
